0246e11d6cdfad8163179d1f2647fb8069db9c01
MPB <mp4learning.com>
2020-07-21 20:52:11 +0530

    Added model class.

--- BuilderDesignPattern/app/src/main/java/com/btk/builderdesignpattern/MainActivity.java
+++ BuilderDesignPattern/app/src/main/java/com/btk/builderdesignpattern/MainActivity.java
@@ -3,6 +3,7 @@ package com.btk.builderdesignpattern;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        User user = new User.builder()
+                .userId("100")
+                .name("username")
+                .address("User Address")
+                .age(30)
+                .build();
+
+        Log.v("===","Name:"+user.getName());
+        Log.v("===","Address:"+user.getAddress());
+        Log.v("===","Age:"+user.getAge());
+        Log.v("===","UserId:"+user.getUserId());
     }
 }
--- BuilderDesignPattern/app/src/main/java/com/btk/builderdesignpattern/User.java
+++ BuilderDesignPattern/app/src/main/java/com/btk/builderdesignpattern/User.java
@@ -2,4 +2,70 @@ package com.btk.builderdesignpattern;
 
 public class User {
 
+    private String name;
+
+    private String userId;
+
+    private String address;
+
+    private int age;
+
+    private User(builder builder) {
+        this.name = builder.name;
+        this.userId = builder.userId;
+        this.address = builder.address;
+        this.age = builder.age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    static class builder {
+
+        private String name;
+
+        private String userId;
+
+        private String address;
+
+        private int age;
+
+        public builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public builder userId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public  builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+
+    }
 }
